9d4236f119b7523242ea79bc30076159f703fb89
TheFrankWei <24798904+TheFrankWei@users.noreply.github.com>
2024-04-07 04:33:53 -0400

    add animations

--- app/admin/Ticket.tsx
+++ app/admin/Ticket.tsx
@@ -5,6 +5,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
 import { CaretDown, CaretUp } from "@phosphor-icons/react";
 import { ErrorMessage } from "@hookform/error-message";
+import { AnimatePresence, motion } from "framer-motion";
 
 enum Status {
   new = "new",
@@ -40,60 +41,72 @@ export default function Ticket(Props: TicketProps) {
 
   return (
     <div className="flex flex-col gap-1 w-full">
-      <div
-        className="flex flex-row justify-between group hover:bg-zealthyNeutralSecondary py-6 px-4"
-        onClick={() => setOpen(!open)}
-      >
-        <div>Ticket Name</div>
-        <div className="group-hover:text-zealthySecondary">
-          {open ? <CaretUp size={24} /> : <CaretDown size={24} />}
-        </div>
-      </div>
-      {open && (
-        <div className="flex flex-col gap-2 px-4 py-4">
-          <div>
-            <div>Status</div>
-            <div>Description</div>
+      <AnimatePresence>
+        <div
+          className="flex flex-row justify-between group hover:bg-zealthyNeutralSecondary py-6 px-4"
+          onClick={() => setOpen(!open)}
+        >
+          <div>Ticket Name</div>
+          <div className="group-hover:text-zealthySecondary">
+            {open ? <CaretUp size={24} /> : <CaretDown size={24} />}
           </div>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-2"
-          >
-            <div className="w-fit">
-              <Select
-                id="status"
-                label="Update Status"
-                options={STATUS_OPTIONS}
-                {...register("status", { required: true })}
-              />
-              <ErrorMessage
-                errors={errors}
-                name="status"
-                render={({ message }) => <p className="error">{message}</p>}
-              />
-            </div>
-            <div>
-              <TextArea
-                id="description"
-                label="Update Description"
-                {...register("description", {
-                  required: { value: true, message: "Description is required" },
-                })}
-              />
-              <ErrorMessage
-                errors={errors}
-                name="description"
-                render={({ message }) => <p className="error">{message}</p>}
-              />
-            </div>
-            <div className="text-end pt-2">
-              <button type="submit" className="button">
-                Submit
-              </button>
-            </div>
-          </form>
         </div>
-      )}
+
+        {open && (
+          <motion.div
+            key="info"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col gap-2 px-4 pt-2 pb-4"
+          >
+            <div>
+              <div>Status</div>
+              <div>Description</div>
+            </div>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-2"
+            >
+              <div className="w-fit">
+                <Select
+                  id="status"
+                  label="Update Status"
+                  options={STATUS_OPTIONS}
+                  {...register("status", { required: true })}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="status"
+                  render={({ message }) => <p className="error">{message}</p>}
+                />
+              </div>
+              <div>
+                <TextArea
+                  id="description"
+                  label="Update Description"
+                  {...register("description", {
+                    required: {
+                      value: true,
+                      message: "Description is required",
+                    },
+                  })}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="description"
+                  render={({ message }) => <p className="error">{message}</p>}
+                />
+              </div>
+              <div className="text-end pt-2">
+                <button type="submit" className="button">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import Input from "@/components/Input";
 import TextArea from "@/components/TextArea";
 import { ErrorMessage } from "@hookform/error-message";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface FormInput {
   name: string;
@@ -23,60 +24,79 @@ export default function Home() {
   };
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="py-6 text-bold">Report an Issue</div>
-      {isSubmitSuccessful? (
-        <div className="text-center">
-          <div className="py-6">Thanks for reporting your issue!</div>
-          <button onClick={() => reset()} className="button">
-            Report Another Issue
-          </button>
-        </div>
-      ) : (
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <Input
-              id="Name"
-              {...register("name", {
-                required: { value: true, message: "Name is required" },
-              })}
-            />
-            <ErrorMessage
-              errors={errors}
-              name="name"
-              render={({ message }) => <p className="error">{message}</p>}
-            />
-          </div>
-          <div>
-            <Input
-              id="Email"
-              {...register("email", {
-                required: { value: true, message: "Email is required" },
-              })}
-            />
-            <ErrorMessage
-              errors={errors}
-              name="email"
-              render={({ message }) => <p className="error">{message}</p>}
-            />
-          </div>
-          <div>
-            <TextArea
-              id="Description"
-              {...register("description", {
-                required: { value: true, message: "Description is required" },
-              })}
-            />
-            <ErrorMessage
-              errors={errors}
-              name="description"
-              render={({ message }) => <p className="error">{message}</p>}
-            />
-          </div>
-          <button type="submit" className="button">
-            Submit
-          </button>
-        </form>
-      )}
+      <AnimatePresence>
+        <div className="py-6 text-bold">Report an Issue</div>
+        {isSubmitSuccessful ? (
+          <motion.div
+            key="success"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="text-center"
+          >
+            <div className="py-6">Thanks for reporting your issue!</div>
+            <button onClick={() => reset()} className="button">
+              Report Another Issue
+            </button>
+          </motion.div>
+        ) : (
+          <motion.div
+            key="form"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div>
+                <Input
+                  id="Name"
+                  {...register("name", {
+                    required: { value: true, message: "Name is required" },
+                  })}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="name"
+                  render={({ message }) => <p className="error">{message}</p>}
+                />
+              </div>
+              <div>
+                <Input
+                  id="Email"
+                  {...register("email", {
+                    required: { value: true, message: "Email is required" },
+                  })}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="email"
+                  render={({ message }) => <p className="error">{message}</p>}
+                />
+              </div>
+              <div>
+                <TextArea
+                  id="Description"
+                  {...register("description", {
+                    required: {
+                      value: true,
+                      message: "Description is required",
+                    },
+                  })}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="description"
+                  render={({ message }) => <p className="error">{message}</p>}
+                />
+              </div>
+              <button type="submit" className="button">
+                Submit
+              </button>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 }
